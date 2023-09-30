a1c928bfcfb0c1b841f7fb77ef39901cd4b10e03
mykola <mykolaivanowski@mail.com>
2023-09-30 21:10:23 +0300

    asynk example add

--- base/async.py
+++ base/async.py
@@ -1,11 +1,10 @@
-import time
 import asyncio
 
 
-async def func1():
-    task = asyncio.create_task(func2(1))
-    task = asyncio.create_task(func2(2))
-    task = asyncio.create_task(func2(3))
+async def main_func():
+    task = asyncio.create_task(func_task(1))
+    task = asyncio.create_task(func_task(2))
+    task = asyncio.create_task(func_task(3))
 
     return_value = await task
     print("A")
@@ -14,10 +13,35 @@ async def func1():
     print(f'returned value: {return_value}')
 
 
-async def func2(n):
+async def func_task(n):
     print("AA"*n)
     await asyncio.sleep(1)
     n = n+1
     print("AA" * n)
 
-asyncio.run(func1())
+asyncio.run(main_func())
+
+
+# example 2
+print("******* example 2 ********")
+
+
+# this works synchronous (incorrect)
+async def main_job():
+    for task in (1,1,3,2):
+        await synk_task(task)
+
+
+# this works correctly asynchronous
+async def main_job():
+    coroutine = [synk_task(task=task) for task in ( 1,1,3,2)]
+    await asyncio.gather(*coroutine)
+
+
+async def synk_task(task):
+    print("task  starts: ",task)
+    await asyncio.sleep(task)
+    print("task finished: ",task)
+
+
+asyncio.run(main_job())
