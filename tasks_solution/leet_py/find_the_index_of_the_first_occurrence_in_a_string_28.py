563b2b57e34c30b293e65d367194006e999000fd
mykola <mykolaivanowski@mail.com>
2024-02-20 22:18:27 +0200

    28

--- tasks_solution/leet_py/find_the_index_of_the_first_occurrence_in_a_string_28.py
+++ tasks_solution/leet_py/find_the_index_of_the_first_occurrence_in_a_string_28.py
@@ -1,4 +1,34 @@
-def str_str(haystack,needle):
-    if len(needle) == 0:
-        return 0
-    return haystack.find(needle)
+
+def find_the_index(haystack, needle):
+    index = 0
+    flag = True
+    for h in range(len(haystack)):
+        if haystack[h] == needle[0]:
+            if len(needle) + h > len(haystack):
+                return -1
+            index = h
+            for n in range(len(needle)):
+                if haystack[h + n] != needle[n]:
+                    flag = False
+                    break
+            if flag == False:
+                flag = True
+            else:
+                return index
+    return -1
+
+
+r = find_the_index("hello", "ll")
+print(r)
+
+
+def alternative(haystack: str, needle: str) -> int:
+    if len(needle) > len(haystack):
+        return -1
+    for i in range(len(haystack) - len(needle) + 1):
+        # Check if substring matches needle
+        if haystack[i:i + len(needle)] == needle:
+            return i
+
+    # If needle is not found
+    return -1
