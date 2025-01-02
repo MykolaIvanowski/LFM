402e686906df5db67d89abd508d3f1af9d51f9a8
mykola <mykolaivanowski@gmail.com>
2025-01-02 23:53:18 +0000

    sql 8weeks 1tq10

--- tasks_solution/8weeksql/case1.sql
+++ tasks_solution/8weeksql/case1.sql
@@ -78,11 +78,47 @@ with first_order as(select
 )
 
 select * from first_order
-where rnk_orders = 1
+where rnk_orders = 1;
 
 -- 7. Which item was purchased just before the customer became a member?
+with purchased_just as(select s.customer_id, s.order_date, mem.join_date, m.product_name,
+	rank() over(partition by s.customer_id order by s.order_date desc) as rnk_date,
+	row_number() over(partition by s.customer_id order by s.order_date desc) as r_date
+from dannys_diner.sales s
+join dannys_diner.members as mem using(customer_id)
+join dannys_diner.menu as m using(product_id)
+where s.order_date < mem.join_date)
+
+select * from purchased_just
+where r_date =1;
 
 -- 8. What is the total items and amount spent for each member before they became a member?
--- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
--- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
+select s.customer_id, count(s.product_id),sum(m.price) from dannys_diner.sales s
+join dannys_diner.members mr using(customer_id)
+join dannys_diner.menu m using(product_id)
+where s.order_date < mr.join_date
+group by s.customer_id;
 
+-- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+select s.customer_id,
+	sum( CASE WHEN m.product_name = 'sushi'
+		 THEN m.price * 20
+    	 ELSE m.price * 10 END) AS points
+from dannys_diner.sales s
+join dannys_diner.menu m using(product_id)
+group by s.customer_id;
+
+-- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
+select
+	s.customer_id,
+	sum(case
+		when s.order_date between m.join_date and (join_date +interval '6 day')
+		then mu.price * 10*2
+		when mu.product_name ='sushi' then mu.price*10*2
+		else mu.price*10 end)
+	as points_first
+from dannys_diner.sales s
+join dannys_diner.members m using(customer_id)
+join dannys_diner.menu mu using(product_id)
+where s.order_date >= m.join_date and s.order_date <='2021-01-31'
+group by s.customer_id
