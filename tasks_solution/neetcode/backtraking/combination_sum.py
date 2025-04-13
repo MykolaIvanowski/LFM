7cf9515cf2a73f4700fa6efe4bbae77bbd126101
mykola <mykolaivanowski@gmail.com>
2025-04-13 23:49:57 +0100

    neet: combination sum 2 [tree]

--- tasks_solution/neetcode/backtraking/combination_sum.py
+++ tasks_solution/neetcode/backtraking/combination_sum.py
@@ -1,5 +1,6 @@
 # given array of numbers and int target value.
-# find how many sets uniq numbers equal to target
+# find how many uniq sets in sum is equal to target
+# each numbers in array can be used multiple times
 # #
 # Input:  nums = [2,5,6,9]    # as you see one numer can be used many times
 #         target = 9
@@ -10,22 +11,35 @@
 # Output: [[3,3,3,3,4],[3,3,5,5],[4,4,4,4],[3,4,4,5]]
 from typing import List
 
-
+# how many branches have this solution?
+# the number of branches in this solution depends on len(nums) and target
+#
+# this solution avoid redundant subset because
+# index control
 class Solution:
     def combination_sum(self, nums: List[int], target: int)-> List[List[int]]:
         res =[]
 
-        def dfs(i , cursor, total):
-            if total == target:
-                res.append(cursor.copy())
+        def dfs(index, possible_sets, sum_possible_sets):
+            if sum_possible_sets == target:
+                # copy reference to result array if sets is match to target
+                res.append(possible_sets.copy())
                 return
-            if i>=len(nums) or total> target:
+                # this if means that we achieve the end of tree
+            if index>=len(nums) or sum_possible_sets> target:
                 return
+            # add numbers, it possible add to result if feet the condition in future calls
+            possible_sets.append(nums[index])
+            # go to left subtree
+            dfs(index, possible_sets, sum_possible_sets + nums[index])
 
-            cursor.append(nums[i])
-            dfs(i, cursor, total+ nums[i])
-            cursor.pop()
-            dfs(i + 1, cursor, total)
+            # when left subtree, (leaf) is finished
+            # then we clear last element and go to right subtree (leaf)
+            possible_sets.pop()
+
+            # go to right subtree, to check right variety fo subtree we need index+1
+            # we go to next number in array for adding it in next recursive call
+            dfs(index + 1, possible_sets, sum_possible_sets)
 
         dfs(0,[],0)
         return res
