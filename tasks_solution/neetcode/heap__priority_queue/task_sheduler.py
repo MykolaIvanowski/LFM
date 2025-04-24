524a98e92e274ff5a770dcc5baef8900133f1519
mykola <mykolaivanowski@gmail.com>
2025-04-24 22:58:59 +0100

    neet: task sheduller [heap]

---
+++ tasks_solution/neetcode/heap__priority_queue/task_sheduler.py
@@ -0,0 +1,50 @@
+# given array with chars, and n. This chars in array  represent task
+# and n represent cooldown between identical tasks.
+# find minimal time required to execute all given task (do not forget about cooldown)
+# array [a,b,c,a,b,c] -> optimal sequence for tasks -> a->b->c->a->b->a  (no cooldown)
+#                    not optimal sequence for tasks -> a->__a->b->__b->c->__c (__ represent cooldown)
+#
+# Input: tasks = ["X","X","Y","Y"], n = 2
+# Explanation: A possible sequence is: X -> Y -> idle -> X -> Y.
+#
+#
+# Input: tasks = ["A","A","A","B","C"], n = 3
+# Output: 9
+# Explanation: A possible sequence is: A -> B -> C -> Idle -> A -> Idle -> Idle -> Idle -> A.
+#
+import heapq
+from collections import Counter, deque
+from typing import List
+
+
+class Solution:
+    def least_interval(self, tasks: List[str], n : int)-> int:
+        # count every task and  store as value
+        # example: task = [a,a,a,b] => count={a:3, b:1}
+        count_hashmap = Counter(tasks) # made hash map
+        max_heap = [-c for c in count_hashmap.values()] # take ints from hashmap and make the negative
+        heapq.heapify(max_heap) # turn into min heap but we will have result as a max heap
+        time  = 0 # result, or time required to execute tasks
+        queue = deque()
+
+        while max_heap or queue:
+            time+=1
+
+            if not max_heap:
+
+                time = queue[0][1]
+            else:
+                #
+                current_task_time = 1 + heapq.heappop(max_heap) # pop value in log n time
+
+                if current_task_time:
+
+                    queue.append([current_task_time, time+n])
+            if queue and queue[0][1] == time:
+                heapq.heappush(max_heap, queue.popleft()[0])
+
+        return time
+
+obj = Solution()
+r = obj.least_interval(["A","A","A","B","C"], n = 3)
+print(r)
