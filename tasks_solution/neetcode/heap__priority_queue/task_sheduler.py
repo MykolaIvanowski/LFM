edfca89dbee05f41f82ab5f96dff50cb1d615ee9
mykola <mykolaivanowski@gmail.com>
2025-04-25 00:39:22 +0100

    neet: task sheduller, add explanation [heap]

--- tasks_solution/neetcode/heap__priority_queue/task_sheduler.py
+++ tasks_solution/neetcode/heap__priority_queue/task_sheduler.py
@@ -2,7 +2,9 @@
 # and n represent cooldown between identical tasks.
 # find minimal time required to execute all given task (do not forget about cooldown)
 # array [a,b,c,a,b,c] -> optimal sequence for tasks -> a->b->c->a->b->a  (no cooldown)
-#                    not optimal sequence for tasks -> a->__a->b->__b->c->__c (__ represent cooldown)
+#                    not optimal sequence for tasks -> a->__a->b->__b->c->__c (__ represent cooldown or
+#                    Idle time refers to periods during task scheduling when the CPU isn't actively
+#                    executing any tasks)
 #
 # Input: tasks = ["X","X","Y","Y"], n = 2
 # Explanation: A possible sequence is: X -> Y -> idle -> X -> Y.
@@ -28,21 +30,31 @@ class Solution:
         queue = deque()
 
         while max_heap or queue:
-            time+=1
+            time+=1 # new iteration new time for task
 
+            # this if means that we do not have other choice as use cooldown
+            # because in pool we have only the same chars (task)
             if not max_heap:
+                time = queue[0][1] # task timer increased to timer + n (n - cooldown the same idle time)
 
-                time = queue[0][1]
             else:
-                #
-                current_task_time = 1 + heapq.heappop(max_heap) # pop value in log n time
-
+                # get max value from (actually min because it is minheap) heap
+                # and decrease it (actually increase because it is minheap)
+                # also crucial that we pop from heap and that means that we do not take it to task again,
+                # until it pushed back to heap
+                current_task_time = 1 + heapq.heappop(max_heap) # for heap pop value in log n time
                 if current_task_time:
-
+                    # put recently taken counter to queue (counter for concrete char),
+                    # and general time for tasks plus cooldown time - this in future help
+                    # find when we should put back value from queue to heap
                     queue.append([current_task_time, time+n])
+
             if queue and queue[0][1] == time:
+                # add task back (char) to heap for next task checks (scheduling)
+                # as we add current_task_time back in next iteration it may be checked (calculated)
                 heapq.heappush(max_heap, queue.popleft()[0])
 
+        # return result
         return time
 
 obj = Solution()
