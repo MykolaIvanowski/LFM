e3ffe4eedfe9825edfe6210e58cfff3fa8f01225
mykola <mykolaivanowski@gmail.com>
2025-06-10 23:55:55 +0100

    django: components explanation short

--- django/relations.py
+++ django/relations.py
@@ -1,4 +1,8 @@
 # models, views, urls, tamplates
+from traceback import format_stack
+
+from pyspark.task_i import username
+
 
 #  models used for definding structure of data base
 class Product(model.Model):
@@ -37,3 +41,65 @@ urlspatterns = [
 # urls - dispatcher connects incomming requests to concrete function or views
 # templates - present data to the user
 # could be changed for - API-driven frontend development
+
+
+        #*******
+
+# Forms
+# form automaticly validates fileds like email and ensures structured data
+
+form django import forms
+
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=180)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
+
+
+# Admin
+# Admin panel allows developers to manage database models through a user frendly interface without sql queries
+
+from django.contrib import admin
+from .models import BlogPost
+
+admin.site.register(BlogPost)  # register the model with admin site
+
+
+# Middleware
+#  middleware function process requests and responses globally across Django
+# authentication checks, logining, modifying response
+
+
+from django.utils.deprecation import MiddlawareMixin
+
+class MyMiddleware(MiddlawareMixin):
+    def process_request(self, requests):
+        f'Middleware processing request'
+
+    def process_response(self, request, reponce):
+        f'Middleware processing response'
+        return reponce
+
+
+# Signals
+# signals allow  decoupled applications to communicate with each ather
+# They trier events then database action occur (pre_save, post_save ,etc)
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import UserProfile
+
+@receiver(post_save, sender=UserProfile):
+def create_profile(sender, instance, created, **kwargs):
+    if create:
+        f'new profile created for {instance.user}'
+
+
+# AuthenticationSystem
+# Django has a build-in-authentication system with user management, password handling, and login/logout views
+
+from django.contrib.auth import authenticate, login
+
+user = authenticate(username='mykola', password='secret')
+if user is not None:
+    login(request, user)
